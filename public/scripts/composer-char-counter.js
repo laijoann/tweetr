3e6e7a3795d04e62dcc487dadb8ff1e4d3d0ee63
Joann Lai <lai.joann.cy@gmail.com>
2017-03-21 17:31:33 +0000

    Refactored tweet character counter feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-    let charCount = 0;
-    $( "textarea" ).keyup(function() {
-      console.log( "Handler for .keypress() called." );
-      console.log(this.value.length);
-      this.parentNode.children[2].innerText = 140 - this.value.length;
-      if (this.value.length > 140) {
-        this.parentNode.children[2].style.color = "red";
-      } else {
-        this.parentNode.children[2].style.color = "black";
-      }
-    });
-
-
+  $( "#tweet-input" ).keyup(function() {
+    const $field = $(this);
+    //if i use ES6 fat arrow, then i would need
+    //var $field = $(event.target)
+    //because this has a different context
+    const inputLength = 140 - $field.val().length;
+    const $textCounter = $field.siblings('.counter');
+    $textCounter.text(inputLength);
+    if (inputLength < 0) {
+      $field.addClass('error');
+      $textCounter.addClass('error');
+    } else {
+      $field.removeClass('error');
+      $textCounter.removeClass('error');
+    }
+    //best practices:
+    //add and remove a css class for reusability
+  });
 });
