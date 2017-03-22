7662c89650708ea6eccb84e24841bf7b085a23b1
Joann Lai <lai.joann.cy@gmail.com>
2017-03-22 20:08:10 +0000

    Added form toggle button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,15 @@
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-} //to prevent cross-site scripting
-
-function createTweetElement (tweetObject) {
-  let $tweet = `<article>
-    <header>
-      <img src="${tweetObject.user.avatars.small}">
-      <h2>${tweetObject.user.name}</h2>
-      <div>${tweetObject.user.handle}</div>
-    </header>
-    <body>
-      <p>
-        ${escape(tweetObject.content.text)}
-      </p>
-    </body>
-    <footer>
-      ${new Date(tweetObject.created_at)}
-    </footer>
-  </article>`;
-  return $tweet;
-}
-
-function renderTweets (tweetData) {
-  let newDOM = "";
-  tweetData.forEach((tweet) => {
-    newDOM += createTweetElement(tweet);
-  })
-  return $(".new-tweet").after(newDOM);
-}
-
 $( document ).ready(function() {
+
+  $( "#compose" ).click(function() {
+    if ($('.new-tweet').is(':visible')) {
+      $('.new-tweet').slideUp();
+    } else {
+      $('.new-tweet').slideDown();
+      $('textarea').focus();
+    }
+
+});
+
   var form = $("form");
   form.on("submit", function(event) {
     event.preventDefault();
@@ -42,7 +20,7 @@ $( document ).ready(function() {
 
     const flashMsg = () => {
       $errMsg.css("visibility", "visible");
-      setTimeout(function () { $errMsg.css("visibility", "hidden"); }, 2000);
+      setTimeout(function () { $errMsg.css("visibility", "hidden"); }, 1500);
     }
 
     let errorCheck = true;
@@ -59,8 +37,6 @@ $( document ).ready(function() {
       flashMsg();
     } else {
 
-      $("textarea").serialize();
-
       $.ajax({
         url: "/tweets",
         type: "POST",
@@ -73,8 +49,6 @@ $( document ).ready(function() {
     }
   }); //prevents browser from leaving page when submit button is clicked
 
-// {"user":{"name":"Descartes","avatars":{"small":"https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png","regular":"https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png","large":"https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"},"handle":"@rd"},"content":{"text":"Je pense , donc je suis"},"created_at":1461113959088}
-
   function loadTweets () {
     $.ajax({
       url: '/tweets',
@@ -85,4 +59,36 @@ $( document ).ready(function() {
 
   loadTweets();
 
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  } //to prevent cross-site scripting
+
+  function createTweetElement (tweetObject) {
+    let $tweet = `<article>
+      <header>
+        <img src="${tweetObject.user.avatars.small}">
+        <h2>${tweetObject.user.name}</h2>
+        <div>${tweetObject.user.handle}</div>
+      </header>
+      <body>
+        <p>
+          ${escape(tweetObject.content.text)}
+        </p>
+      </body>
+      <footer>
+        ${new Date(tweetObject.created_at)}
+      </footer>
+    </article>`;
+    return $tweet;
+  }
+
+  function renderTweets (tweetData) {
+    let newDOM = "";
+    tweetData.forEach((tweet) => {
+      newDOM += createTweetElement(tweet);
+    })
+    return $(".new-tweet").after(newDOM);
+  }
 })
