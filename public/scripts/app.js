ac256dfa601fc678ea89ac957151dcf9723fd9bf
Joann Lai <lai.joann.cy@gmail.com>
2017-03-22 18:31:00 +0000

    Added form validation function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,62 @@
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+} //to prevent cross-site scripting
+
+function createTweetElement (tweetObject) {
+  let $tweet = `<article>
+    <header>
+      <img src="${tweetObject.user.avatars.small}">
+      <h2>${tweetObject.user.name}</h2>
+      <div>${tweetObject.user.handle}</div>
+    </header>
+    <body>
+      <p>
+        ${escape(tweetObject.content.text)}
+      </p>
+    </body>
+    <footer>
+      ${new Date(tweetObject.created_at)}
+    </footer>
+  </article>`;
+  return $tweet;
+}
+
+function renderTweets (tweetData) {
+  let newDOM = "";
+  tweetData.forEach((tweet) => {
+    newDOM += createTweetElement(tweet);
+  })
+  return $('.container').append(newDOM);
+}
+
 $( document ).ready(function() {
-
-  function escape(str) {
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  } //to prevent cross-site scripting
-
-  function createTweetElement (tweetObject) {
-    let $tweet = `<article>
-      <header>
-        <img src="${tweetObject.user.avatars.small}">
-        <h2>${tweetObject.user.name}</h2>
-        <div>${tweetObject.user.handle}</div>
-      </header>
-      <body>
-        <p>
-          ${escape(tweetObject.content.text)}
-        </p>
-      </body>
-      <footer>
-        ${new Date(tweetObject.created_at)}
-      </footer>
-    </article>`;
-    return $tweet;
-  }
-
-  function renderTweets (tweetData) {
-    let newDOM = "";
-    tweetData.forEach((tweet) => {
-      newDOM += createTweetElement(tweet);
-    })
-    return $('.container').append(newDOM);
-  }
 
   $("form").on("submit", function(event) {
     event.preventDefault();
-    $("textarea").serialize();
+
+    const $errMsg = $("#errorMsg")
+    const $submit = $("input");
+    const $counter = parseInt($(".counter").text());
+
+    let errorCheck = true;
+    if ($counter === 140) {
+      errorCheck = "empty";
+    } else if ($counter < 0) {
+      errorCheck = "tooLong";
+    }
+    if (errorCheck === "empty") {
+      $errMsg.css("visibility", "visible");
+      $errMsg.text("FUck your empty tweet");
+      setTimeout(function () {$errMsg.css("visibility", "hidden")}, 2000);
+    } else if (errorCheck === "tooLong") {
+      $errMsg.css("visibility", "visible");
+      $errMsg.text("too fuking long u idiot")
+      setTimeout(function () {$errMsg.css("visibility", "hidden")}, 2000);
+    } else {
+      $("textarea").serialize();
+    }
   }); //prevents browser from leaving page when submit button is clicked
 
   function loadTweets () {
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,21 @@
+/* the submit.prop('disabled' ...) lines are my personal preferred UX for indicating to users that their tweets are invalid. But for the purpose of submitting this project, I have implemented the required flash messages in app.js instead */
 $( document ).ready(function() {
   $( "#tweet-input" ).keyup(function() {
     const $field = $(this);
-    //if i use ES6 fat arrow, then i would need
-    //var $field = $(event.target)
-    //because this has a different context
-    const inputLength = 140 - $field.val().length;
+    const $submit = $("input");
+    const charRemain = 140 - $field.val().length;
     const $textCounter = $field.siblings('.counter');
-    $textCounter.text(inputLength);
-    if (inputLength < 0) {
+    $textCounter.text(charRemain);
+    if (charRemain === 140) {
+      //$submit.prop('disabled', true);
+    } else if (charRemain < 0) {
       $field.addClass('error');
       $textCounter.addClass('error');
+      //$submit.prop('disabled', true);
     } else {
       $field.removeClass('error');
       $textCounter.removeClass('error');
+      //$submit.prop('disabled', false);
     }
-    //best practices:
-    //add and remove a css class for reusability
   });
 });
